3e9321b35f2e50eef7653e1c4b77412a3fb5a63e
RawGate <rawabi98r@gmail.com>
2024-05-28 09:46:22 +0300

    updated api

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,7 +4,7 @@ const isDevelopment = import.meta.env.MODE === 'development'
 let baseURL = "https://backend-ecommerce-project-service.onrender.com/api"
 
 if (!isDevelopment) {
-  baseURL = 'http://localhost:5110/api'
+  baseURL = "https://backend-ecommerce-project-service.onrender.com/api"
 }
 
 const api = axios.create({
@@ -12,12 +12,12 @@ const api = axios.create({
 })
 
 // use this to handle errors gracefully
- api.interceptors.response.use(
+ /* api.interceptors.response.use(
   (response) => response,
  (error) => {
    if (error.response.status === 500) {
     throw new Error(error.response.data)
   }
-  })
+  }) */
 
 export default api
